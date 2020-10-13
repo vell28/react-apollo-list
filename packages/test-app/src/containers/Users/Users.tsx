95bc1607aee079429ff8290c311507c61c2c3479
Valik Ivanov <valentin28.web@gmail.com>
2020-10-13 13:16:38 +0300

    added fetch error handler

--- packages/test-app/src/containers/Users/Users.tsx
+++ packages/test-app/src/containers/Users/Users.tsx
@@ -1,44 +1,52 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { useQuery } from '@apollo/client';
 import { IUser } from '../../models';
 
 import { GET_USERS } from '../../graphql/queries/users';
 import { Table, NUMBER_OF_PAGE, PageProps, SortProps } from '../../components/Table';
-import { ERROR_TEXT } from './const';
+import { ERROR_TEXT, FETCH_ERROR_TEXT } from './const';
 import './styles.css';
 
 export const Users = () => {
+  const [fetchError, setFetchError] = useState<boolean>(false);
 
   const { loading, error, data, fetchMore } = useQuery(GET_USERS, {
-    variables: { field: '', order: '', offset: 0, limit: NUMBER_OF_PAGE }
+    variables: { field: '', order: '', offset: 0, limit: NUMBER_OF_PAGE },
   });
 
-  const handleFetchMore = ({ field, order, offset, limit }: PageProps & SortProps) => {
-    fetchMore({
-      variables: { field, order, offset, limit },
-      updateQuery: (
-        prev: { users: {usersList: IUser[], total: number} }, 
-        props: { fetchMoreResult?: { users: {usersList: IUser[], total: number} } }
-      ) => {
-        const { fetchMoreResult } = props;
-
-        if (!fetchMoreResult) return prev;
-        if (offset === 0) return fetchMoreResult;
-
-        return { 
-          users: {
-            ...fetchMoreResult.users, 
-            usersList: [...prev.users.usersList, ...fetchMoreResult.users.usersList] 
-          }
-        };
-      },
-    })
-  };
+  const handleFetchMore = useCallback( 
+    async ({ field, order, offset, limit }: PageProps & SortProps) => {
+      if (fetchError) setFetchError(!fetchError);
+      try {
+        await fetchMore({
+          variables: { field, order, offset, limit },
+          updateQuery: (
+            prev: { users: {usersList: IUser[], total: number} }, 
+            props: { fetchMoreResult?: { users: {usersList: IUser[], total: number} } }
+          ) => {
+            const { fetchMoreResult } = props;
+    
+            if (!fetchMoreResult) return prev;
+            if (offset === 0) return fetchMoreResult;
+    
+            return { 
+              users: {
+                ...fetchMoreResult.users, 
+                usersList: [...prev.users.usersList, ...fetchMoreResult.users.usersList] 
+              }
+            };
+          },
+        })
+      } catch(e) {
+        setFetchError(true);
+      }
+  }, [fetchError, fetchMore]);
 
   return (
     <div className="container">
       {error && <div className='error tableError'>{ERROR_TEXT}</div>}
       <Table data={data} onFetchMore={handleFetchMore} loading={loading || !data || Boolean(error)} />
+      {fetchError && <div className='error tableError'>{FETCH_ERROR_TEXT}</div>}
     </div>
   )
 };
--- packages/test-app/src/containers/Users/const.ts
+++ packages/test-app/src/containers/Users/const.ts
@@ -1,1 +1,2 @@
-export const ERROR_TEXT: string = 'Something went wrong. Please reload page'
+export const ERROR_TEXT: string = 'Something went wrong. Please reload page';
+export const FETCH_ERROR_TEXT: string = 'Something went wrong. Try again';
